9fb3cd1d6ee9b0c80eaa3a733a202625aee7ab23
fmausberg <felix.mausberg@gmail.com>
2025-02-04 16:11:10 +0100

    Initial load of backend

---
+++ recruitingtests-backend/src/main/java/net/mausberg/recruitingtests/RecruitingtestsApplication.java
@@ -0,0 +1,13 @@
+package net.mausberg.recruitingtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingtestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitingtestsApplication.class, args);
+	}
+
+}
---
+++ recruitingtests-backend/src/test/java/net/mausberg/recruitingtests/RecruitingtestsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mausberg.recruitingtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitingtestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
